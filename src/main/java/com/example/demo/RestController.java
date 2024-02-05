b400d05152e16f27e239b14e915a98cc28da1cd5
abdullah-jaffer <abdullahjaffer96@gmail.com>
2024-02-06 04:57:01 +0500

    added demo

---
+++ src/main/java/com/example/demo/RestController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping
+public class RestController {
+
+
+    @GetMapping("/hello")
+    public String ping() {
+        return "Hello World";
+    }
+}
